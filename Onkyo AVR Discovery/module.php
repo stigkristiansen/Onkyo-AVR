1298f646157c09bbc1772a890a418dd65cbf4972
stigkristiansen <stigkristiansen@outlook.com>
2025-03-07 21:16:23 +0100

    .

--- Onkyo AVR Discovery/module.php
+++ Onkyo AVR Discovery/module.php
@@ -1,23 +1,36 @@
 <?php
 
 declare(strict_types=1);
-	class OnkyoAVRDiscovery extends IPSModule
-	{
-		public function Create()
-		{
-			//Never delete this line!
-			parent::Create();
-		}
 
-		public function Destroy()
-		{
-			//Never delete this line!
-			parent::Destroy();
-		}
+require_once __DIR__ . '/../libs/ISCP.php';
 
-		public function ApplyChanges()
-		{
-			//Never delete this line!
-			parent::ApplyChanges();
-		}
+class OnkyoAVRDiscovery extends IPSModule {
+		
+	public function Create() {
+		//Never delete this line!
+		parent::Create();
 	}
+
+	public function Destroy() {
+		//Never delete this line!
+		parent::Destroy();
+	}
+
+	public function ApplyChanges() {
+		//Never delete this line!
+		parent::ApplyChanges();
+	}
+
+	private function InitiateDiscovery()	{
+
+		$api = new ISCPCommand('ECN', 'QSTN', '!x');
+		$this->SendDataToParent(json_encode(['DataID' => '{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}', 'Buffer' => $api->ToString()]));
+	}
+
+	public function ReceiveData($JSONString) {
+		$data = json_decode($JSONString);
+		IPS_LogMessage('Received discovery data', $JSONString);
+
+	}
+
+}
---
+++ libs/ISCP.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+class ISCPCommand {
+    public $Command;
+    public $Data;
+
+    private $Prefix = '';
+
+    private $BoolValueMapping = [
+        false => '00',
+        true  => '01',
+        ];
+    
+    public function __construct(string $Command = null, string $Data = null, string $Prefix = '!1') {
+        if ($Command === null) {
+            return;
+        }
+
+        $this->Prefix = $Prefix;
+        
+        if ($Data !== null) {
+            $this->Command = $Command;
+            $this->Data = $Data;
+            
+            return;
+        }
+
+        if ($Command[strlen($Command) - 1] === "\x1A") {
+            $this->Command = substr($Command, 2, 3);
+            $this->Data = substr($Command, 5, -1);
+
+            return;
+        } 
+
+    }
+
+    public function ToString() {
+        if (is_bool($this->Data)) {
+            $Value = $this->BoolValueMapping[$this->Data];
+        } elseif (is_int($this->Data)) {
+            $Value = sprintf('%02X', $this->Data);
+        } else {
+            $Value = $this->Data;
+        }
+
+        $payload = $this->Prefix . $this->Command . $Value . "\r\n";
+        $payloadLen = pack('N', strlen($payload));
+        
+        $ISCPHeader = $payloadLen . "\x01\x00\x00\x00";
+        $ISCPHeaderLen = pack('N', strlen($ISCPHeader) + 8);
+        
+        return 'ISCP' . $ISCPHeaderLen . $ISCPHeader . $payload;
+     }
+
+     public function ToJSON() {
+
+        $data = [
+            'Command' => $this->Command,
+            'Data' => $this->Data
+        ];
+
+        return json_encode($data);
+     }
+
+}
+
