8ce4f26ae28bfe0b74ca3c99878bd3c0b3c5f75d
stigkristiansen <stigkristiansen@outlook.com>
2025-03-08 19:29:00 +0100

    Update module.php

--- Onkyo AVR Discovery/module.php
+++ Onkyo AVR Discovery/module.php
@@ -29,7 +29,7 @@ class OnkyoAVRDiscovery extends IPSModule {
 	}
 	
 	public function GetConfigurationForParent() {
-		return '{"BindIP":"","BindPort":1234,"EnableBroadcast":true,"EnableReuseAddress":true,"Host":"'.gethostbyname(gethostname()).'","Open":true,"Port":60128}';
+		return '{"BindIP":"'.gethostbyname(gethostname()).'","BindPort":1234,"EnableBroadcast":true,"EnableReuseAddress":true,"Host":"","Open":true,"Port":60128}';
 	}
 
 	public function GetConfigurationForm() {
