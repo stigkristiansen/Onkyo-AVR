20d7501a7a3ca18951c4778a19c6f15d3bae12e0
stigkristiansen <stigkristiansen@outlook.com>
2025-03-11 18:09:58 +0100

    Update module.php

--- Onkyo AVR Device/module.php
+++ Onkyo AVR Device/module.php
@@ -32,7 +32,7 @@ class OnkyoAVRDevice extends IPSModule {
 	public function Send() {
 		$command = [
 			'APICommand' => 'PWR',
-			'Data'		 => true;
+			'Data'		 => true
 		];
 
 		$this->SendDataToParent(json_encode(['DataID' => '{1CEDE467-DFFC-5466-5CDF-BBCA3966E657}', 'Buffer' => $command]));
