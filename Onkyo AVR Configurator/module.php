4be3da7321c922cf050a899e79031fe5c078b743
stigkristiansen <stigkristiansen@outlook.com>
2025-04-02 22:46:57 +0200

    Update module.php

--- Onkyo AVR Configurator/module.php
+++ Onkyo AVR Configurator/module.php
@@ -16,6 +16,8 @@ class OnkyoAVRConfigurator extends IPSModule {
 
 		$this->RequireParent('{CD39A489-D759-1786-1904-879A571231AF}');
 		$this->SetReceiveDataFilter("NeverReceiveData");
+
+		$this->SetBuffer(Capabilities::BUFFER, serialize([]));
 	}
 
 	public function Destroy() {
@@ -50,14 +52,36 @@ class OnkyoAVRConfigurator extends IPSModule {
 			return json_encode($form);
 		}
 
+		$instances = $this->GetInstances();
 
-		$this->GetCapabilities();
-
+		$zones = $this->GetCapabilities();
 
 	}
 
-	private function GetCapabilities() {
+	private function GetCapabilities() : array {
 		IPS_Sleep(2000);
+		
 		$this->ExecuteCommand('CAP', 'QSTN');
+		$capabilities = unserialize($this->GetBuffer(Capabilities::BUFFER));
+		
+		$return $capabilities['ZoneList'];
+		
+	}
+	
+	private function GetInstances () : array {
+		$instances = [];
+
+		$this->SendDebug(__FUNCTION__, 'Searching for existing instances of Onkyo devices...', 0);
+
+		$instanceIds = IPS_GetInstanceListByModuleID('{FF80DAC2-0BF3-6A70-F4A8-84A6DE34FDBA}');
+		
+		foreach ($instanceIds as $instanceId) {
+			$instances[$instanceId] = IPS_GetProperty($instanceId, 'MacAddress');
+		}
+
+		$this->SendDebug(__FUNCTION__, sprintf('Found %d existing instance(s) of Onkyo devices', count($instances)), 0);
+		$this->SendDebug(__FUNCTION__, 'Finished searching for existing Onkyo devices', 0);	
+
+		return $instances;
 	}
 }
