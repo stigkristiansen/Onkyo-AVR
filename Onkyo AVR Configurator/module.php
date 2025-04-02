142c9afc24eaf175acd58366bcd9cf79bf049cdf
stigkristiansen <stigkristiansen@outlook.com>
2025-04-02 20:29:58 +0200

    Update module.php

--- Onkyo AVR Configurator/module.php
+++ Onkyo AVR Configurator/module.php
@@ -1,10 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 require_once __DIR__ . '/../libs/capabilities.php';
 require_once __DIR__ . '/../libs/semaphoreHelper.php';
 require_once __DIR__ . '/../libs/miscHelper.php';
-
-declare(strict_types=1);
 	
 class OnkyoAVRConfigurator extends IPSModule {
 	public function Create() {
